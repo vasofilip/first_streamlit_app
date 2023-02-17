e189513d2fc6d586be375a70e56519bf4a9ffdd1
vasofilip <125684885+vasofilip@users.noreply.github.com>
2023-02-17 18:11:14 +0100

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Rastafaraita')
+streamlit.headrer('Rastafaraita')
+streamlit.text('Rastafaraita')
+streamlit.text('Rastafaraita')
+streamlit.text('Rastafaraita')
