a2becb250b0866538cba7d524c03e2532eaa2112
vasofilip <125684885+vasofilip@users.noreply.github.com>
2023-02-21 11:21:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('Rastafaraita')
-streamlit.header('Rastafaraita')
-streamlit.text('Rastafaraita')
-streamlit.text('Rastafaraita')
-streamlit.text('Rastafaraita')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
